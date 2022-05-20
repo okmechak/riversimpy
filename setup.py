ffd7b23d1f9f36c97e7f6dc0cf6cd30ba88e1254
Oleg Kmechak <ofcrashbash@gmail.com>
2022-05-20 13:29:46 +0200

    update download_url and dependencies

--- setup.py
+++ setup.py
@@ -9,20 +9,14 @@ setup(
   author = 'OLEG KMECHAK',                   # Type in your name
   author_email = 'oleg.kmechak@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/okmechak/riversimpy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/okmechak/riversimpy/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/okmechak/riversimpy/archive/refs/tags/v0.1.tar.gz',    # I explain this later on
   keywords = ['LAPLACEA', 'RIVER', 'FEM', 'HARMONIC', 'NETWORK', 'GROWTH', 'SIMULATION'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
+  install_requires=[ ]  ,
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.8',
   ],
 )
