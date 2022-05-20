27f435c03a6f33fc4f03a649d5db6da8932d3850
Oleg Kmechak <ofcrashbash@gmail.com>
2022-05-20 12:46:18 +0200

    almost working riversimpy library

---
+++ README.md
@@ -0,0 +1,4 @@
+#RIVERSIM
+
+Simulation of river growth using model based on Laplace equation[1].
+Mathematicaly, in this program we solve PDE equation using Finite Element Method(FEM). And as result program produces VTK file, which contains solution and its details, which furthere can be viewed in [__ParaView__](https://www.paraview.org/) 
---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+  name = 'RIVERSIS',         # How you named your package folder (MyLib)
+  packages = ['RIVERSIM'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Simulation of river growth using model based on Laplace equation. Mathematicaly, in this program we solve PDE equation using Finite Element Method(FEM).',   # Give a short description about your library
+  author = 'OLEG KMECHAK',                   # Type in your name
+  author_email = 'oleg.kmechak@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/okmechak/riversimpy',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/okmechak/riversimpy/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['LAPLACEA', 'RIVER', 'FEM', 'HARMONIC', 'NETWORK', 'GROWTH', 'SIMULATION'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
